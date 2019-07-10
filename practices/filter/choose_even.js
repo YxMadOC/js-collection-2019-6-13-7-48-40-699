1038ace2678403a3fb91a8f062cb0daafcf38871
CAYDE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUCA6@OOCL.COM>
2019-07-10 14:25:50 +0800

    finished choose_even and choose_multiples_of_three

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function choose_even(collection) {
-
   //implement here
+  const condition = (number) => number % 2 === 0;
+  return collection.filter(condition);
 }
 
 module.exports = choose_even;
