a023111d78914c74c9ec3063ef6a30cd48af82d3
CAYDE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUCA6@OOCL.COM>
2019-07-10 14:50:59 +0800

    finished grouping_count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,13 @@
 function grouping_count(collection) {
 
   //implement here
+  return collection.reduce((prev, item) => {
+    if(item in prev)
+      prev[item] ++;
+    else
+      prev[item] = 1;
+    return prev;
+  }, {});
 }
 
 module.exports = grouping_count;
